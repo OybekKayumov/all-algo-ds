43f3336c65e1e0daddb1d2273a1084a356919c85
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-07-09 07:14:16 +0500

    add stack balanced or not

--- stack-lifo/stack-lifo.cpp
+++ stack-lifo/stack-lifo.cpp
@@ -5,15 +5,24 @@ using namespace std;
 
 int main()
 {
-  stack<int> st;
-  st.push(2);
-  st.push(7);
-  cout << st.top(); // 7
-  st.pop();
-  st.push(3);
-  st.push(4);
-  cout << st.top(); // 4
-  st.pop();
-  cout << st.top(); // 3
-  st.pop();
+  // stack<int> st;
+  // st.push(2);
+  // st.push(7);
+  // cout << st.top(); // 7
+  // st.pop();
+  // st.push(3);
+  // st.push(4);
+  // cout << st.top(); // 4
+  // st.pop();
+  // cout << st.top(); // 3
+  // st.pop();
+
+  //(){}[], balanced or not
+  string s1 = "()({[]}())"; // true
+  string s2 = "{}";         // true
+  string s3 = "}{";         // false
+  string s4 = "";           // true
+  string s5 = "(()";        // false
+  string s6 = "())";        // false
+  string s7 = "[(])";       // false
 }
