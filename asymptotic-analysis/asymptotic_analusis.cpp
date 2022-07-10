b0b9e42a2b6c53b685a30b1cf392c0ca767ea4e6
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-07-10 12:53:13 +0500

    add cycles of O(n) and O(1)

--- asymptotic-analysis/asymptotic_analusis.cpp
+++ asymptotic-analysis/asymptotic_analusis.cpp
@@ -27,6 +27,19 @@ void f2_swap(int &a, int &b)
   b = t;
 }
 
+void f3_cycle_1() // O(1), c = 4
+{
+  const int c = 4;
+  for (int i = 0; i < c; ++i)
+    cout << i;
+}
+
+void f4_cycle_n(int n) // O(n)
+{
+  for (int i = 0; i < n; ++i)
+    cout << i;
+}
+
 int min()
 {
 }
