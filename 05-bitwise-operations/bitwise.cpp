bdaf094ae021c9e569d81487a79ac6a43ed19ac4
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-07-24 15:13:50 +0500

    add bitwise

--- 05-bitwise-operations/bitwise.cpp
+++ 05-bitwise-operations/bitwise.cpp
@@ -7,4 +7,30 @@ int main()
   cout << (51 & 25) << endl; // 17
   cout << (51 | 25) << endl; // 59
   cout << (51 ^ 25) << endl; // 42
+  cout << (~23) << endl;     // -24
+  cout << (~23u) << endl;    // 4294967272
+
+  cout << endl;
+  cout << 12 << 2 << endl;   // 12 2
+  cout << (12 << 2) << endl; // 48
+  cout << (12 >> 2) << endl; // 3
+
+  cout << (1 << 4) << endl; // 16
+  cout << (1 << 5) << endl; // 32
+  cout << (1 << 6) << endl; // 64
+  cout << (1 << 7) << endl; // 128
+
+  int n = 17; // 0...000010001
+  int i = 2;
+  n = n | (1 << i);
+  cout << n << endl; // 21
+  n = n | (1 << i);
+  cout << n << endl; // 21
+  n = n & ~(1 << i);
+  cout << n << endl; // 17
+
+  n = n ^ (1 << i);
+  cout << n << endl; // 21
+
+  // if (n & (1 << i))
 }
