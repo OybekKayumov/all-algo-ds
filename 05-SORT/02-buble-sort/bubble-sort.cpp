b647dbe0ac290388d4b125543cb5b256fa2da40f
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-08-13 16:12:51 +0500

    bubble sort

---
+++ 05-SORT/02-buble-sort/bubble-sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+void bubble_sort(vector<int> &arr)
+{
+  for (int j = 0; j < arr.size(); ++j)
+  {
+
+    for (int i = 1; i < arr.size(); ++i)
+    {
+      if (arr[i] < arr[i - 1])
+      {
+        swap(arr[i], arr[i - 1]);
+      }
+    }
+  }
+}
+
+int main()
+{
+  vector<int> arr;
+  const int n = rand() % 100;
+  for (int i = 0; i < n; ++i)
+    arr.push_back(rand());
+  bubble_sort(arr);
+
+  vector<int> arr_copy = arr;
+  sort(arr_copy.begin(), arr_copy.end());
+
+  std::cout << (arr_copy == arr);
+}
