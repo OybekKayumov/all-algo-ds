01b910c9c869667c5fc17e7cc2123291264d7352
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-08-14 19:11:03 +0500

    add counting sort

--- 05-SORT/04-counting-sort/couning-sort.cpp
+++ 05-SORT/04-counting-sort/couning-sort.cpp
@@ -6,6 +6,29 @@ using namespace std;
 
 void counting_sort(vector<int> &arr)
 {
+  int minimum = arr[0];
+  int maximum = arr[0];
+  for (int i = 0; i < arr.size(); ++i)
+  {
+    if (arr[i] > maximum)
+      maximum = arr[i];
+    if (arr[i] < minimum)
+      minimum = arr[i];
+  }
+  vector<int> bucket(maximum - minimum + 1);
+  for (int i = 0; i < arr.size(); ++i)
+  {
+    bucket[arr[i] - minimum]++;
+  }
+  arr.clear();
+  for (int i = 0; i < bucket.size(); ++i)
+  {
+    int count = bucket[i]; // i + minimum
+    for (int j = 0; j < count; ++j)
+    {
+      arr.push_back(i + minimum);
+    }
+  }
 }
 
 int main()
